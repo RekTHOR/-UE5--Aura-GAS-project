e37d2041ec275609366a9d8493f39116ea084ed0
Csaba Sinkó <sinko.csabaa@gmail.com>
2024-07-03 10:35:04 +0200

    Gameplay tags

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Copyright GameChangerzZz
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improve Block Chance"));
+}
