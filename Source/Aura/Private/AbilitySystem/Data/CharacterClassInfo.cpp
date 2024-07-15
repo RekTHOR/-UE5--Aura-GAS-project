b588bf0556b3b7bb47d311cdd71fd1113570a5c8
Csaba Sinkó <sinko.csabaa@gmail.com>
2024-07-15 18:19:08 +0200

    Removed from git ignore

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,8 @@
+// Copyright GameChangerzZz
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
